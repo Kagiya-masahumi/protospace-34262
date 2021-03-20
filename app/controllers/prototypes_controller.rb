6239e0759d70ef98c191b002481e8319af837e0f
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-03-20 22:04:10 +0900

    投稿機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,4 +2,26 @@ class PrototypesController < ApplicationController
   def index
     
   end
+
+  def new
+    @prototype = Prototype.new(params[:id])
+  end
+
+
+  def create
+    if
+    Prototype.create(prototype_params)
+      redirect_to root_path
+    else
+      render :new
+    end
+
+  end
+
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:image,:title,:catch_copy,:concept).merge(user_id: current_user.id)
+  end
+
 end
--- db/migrate/20210320055724_create_prototypes.rb
+++ db/migrate/20210320055724_create_prototypes.rb
@@ -1,9 +1,13 @@
 class CreatePrototypes < ActiveRecord::Migration[6.0]
   def change
     create_table :prototypes do |t|
-      t.string :title, foreign_key: true
-      t.text :catch_copy, foreign_key: true
-      t.text :concept, foreign_key: true
+      t.string :title
+      t.text :catch_copy
+      t.text :concept
+
+      t.references :user, foreign_key: true
+
+
 
       t.timestamps
     end
