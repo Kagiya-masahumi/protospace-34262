ff8d78294842a880b6af13b9bc1bfe17405f318f
Kagiya-masahumi <Kagiya-masahumi@users.noreply.github.com>
2021-03-21 19:16:17 +0900

    編集・削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   def index
-    
+    @prototypes = Prototype.all
   end
 
   def new
@@ -9,14 +9,38 @@ class PrototypesController < ApplicationController
 
 
   def create
-    if
-    Prototype.create(prototype_params)
+    if Prototype.create(prototype_params)
       redirect_to root_path
     else
       render :new
     end
+  end
+
+  def show
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def edit
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path, method: :get
+    else
+      render :edit
+    end
 
   end
+
+
+  def destroy
+    prototype = Prototype.find(params[:id])
+    prototype.destroy
+    redirect_to root_path
+  end
+
 
 
   private
